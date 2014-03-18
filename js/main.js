7b47ed258fd7cf25dae697ecdc789ab2f1557583
mrcogollor <mrcogollor@btob.es>
2014-03-18 16:32:16 +0100

    front camera

--- js/main.js
+++ js/main.js
@@ -1,23 +1,24 @@
 var app = {
 
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
+    renderHomeView: function() {
+       $('body').html(this.homeTpl());
     },
 
+    // initialize: function() {
+    //     this.store = new MemoryStore();
+    //     $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    // }
     initialize: function() {
-        this.store = new MemoryStore();
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+        var self = this;
+        this.homeTpl = Handlebars.compile($("#home-tpl").html());
     }
-
 };
 
 app.initialize();
