cd67e3276b10db004707a64dc78235600f94098a
mrcogollor <mrcogollor@btob.es>
2014-03-19 10:55:14 +0100

    c

--- js/homeView.js
+++ js/homeView.js
@@ -13,7 +13,7 @@ var HomeView = function() {
     this.changePicture = function(event) {
 	    event.preventDefault();
 	    if (!navigator.camera) {
-	        app.showAlert(navigator.camera, "Error");
+	        app.showAlert("La camara no funciona", "Error");
 	        return;
 	    }
 	    var options =   {   quality: 50,
