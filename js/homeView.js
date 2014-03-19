9bb871bf170772d9f109be4172ea1d4c66e59ba0
mrcogollor <mrcogollor@btob.es>
2014-03-19 13:57:09 +0100

    a

--- js/homeView.js
+++ js/homeView.js
@@ -19,7 +19,8 @@ var HomeView = function() {
 	    var options =   {   quality: 50,
 	                        destinationType: Camera.DestinationType.DATA_URL,
 	                        sourceType: 1,      // 0:Photo Library, 1=Camera, 2=Saved Photo Album
-	                        encodingType: 0     // 0=JPG 1=PNG
+	                        encodingType: 0, // 0=JPG 1=PNG
+	                        direction: 1     
 	                    };
 	 
 	    navigator.camera.getPicture(
