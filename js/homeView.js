a9379dc919b9417716a2ada6ea1cdfefdb0bb5d4
mrcogollor <mrcogollor@btob.es>
2014-03-19 10:54:00 +0100

    c

--- js/homeView.js
+++ js/homeView.js
@@ -18,7 +18,7 @@ var HomeView = function() {
 	    }
 	    var options =   {   quality: 50,
 	                        destinationType: Camera.DestinationType.DATA_URL,
-	                        sourceType: 0,      // 0:Photo Library, 1=Camera, 2=Saved Photo Album
+	                        sourceType: 1,      // 0:Photo Library, 1=Camera, 2=Saved Photo Album
 	                        encodingType: 0     // 0=JPG 1=PNG
 	                    };
 	 
